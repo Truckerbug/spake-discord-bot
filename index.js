e12bac4528c1dc666cdf28950985e31f5864f530
Spencer Schiminsky <89555522+Truckerbug@users.noreply.github.com>
2023-01-06 21:45:14 -0800

    Update index.js

--- index.js
+++ index.js
@@ -13,25 +13,6 @@ for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
     client.commands.set(command.data.name, command);
-}
-
-client.once(Events.ClientReady, () => {
-    console.log('Ready!');
-});
-
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-
-    const command = client.commands.get(interaction.commandName);
-
-    if (!command) return;
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-    }
-});
+};
 
 client.login(token);
